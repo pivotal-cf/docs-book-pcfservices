98169aa2d1c081c856b01ce447928b650bfd8fef
Ben Tarnoff <btarnoff@pivotal.io>
2016-08-26 11:27:59 -0700

    add redirect for metrics[#129162425]

--- redirects.rb
+++ redirects.rb
@@ -78,5 +78,5 @@ r301 %r{/mobile/data/(.*)}, 'https://network.pivotal.io/products/p-data-sync'
 r301 '/mobile/index.html', 'https://network.pivotal.io/products/api-gateway'
 r301 '/mobile/', 'https://network.pivotal.io/products/api-gateway'
 
-
+r301 %r{/pcf-metrics/(.*)}, '/pcf-metrics/1-1/$1'
 
