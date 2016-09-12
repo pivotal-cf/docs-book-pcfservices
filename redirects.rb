c2853358f39613b5d49cdcb5eaef05aae8f79536
Ben Tarnoff <btarnoff@pivotal.io>
2016-09-12 14:49:31 -0700

    add redirect for scs

--- redirects.rb
+++ redirects.rb
@@ -8,7 +8,7 @@ r301 %r{.*}, 'http://docs-pcf-staging.cfapps.io$&', if: Proc.new {|rack_env|
 
 r301 %r{/pivotalcf/(?![\d-]+)(.*)}, "/pivotalcf/1-7/$1"
 r301 %r{/redis/(?![\d-]+)(.*)}, "/redis/1-5/$1"
-r301 %r{/redis/(?![\d-]+)(.*)}, "/redis/1-7/$1"
+r301 %r{/spring-cloud-services/(?![\d-]+)(.*)}, "/spring-cloud-services/1-7/$1"
 r301 %r{/p-mysql/(?![\d-]+)(.*)}, "/p-mysql/1-7/$1"
 r301 %r{/pcf-metrics/(?![\d-]+)(.*)}, "/pcf-metrics/1-1/$1"
 r301 %r{/rabbitmq-cf/(?![\d-]+)(.*)}, "/rabbitmq-cf/1-6-6/$1"
