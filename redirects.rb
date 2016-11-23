9146936de300c5577f23dac608e03e5782dfaa59
animatedmax <mhufnagel@pivotal.io>
2016-11-23 12:28:34 -0800

    makes redirects for rabbitmq-cf/ more flexible

--- redirects.rb
+++ redirects.rb
@@ -29,7 +29,10 @@ r301 %r{/pcf-release-notes/(.*)}, '/pivotalcf/1-8/pcf-release-notes/$1'
 r301 %r{/redis/(?![\d-]+)(.*)}, "/redis/1-6/$1"
 r301 %r{/p-identity/(?![\d-]+)(.*)}, "/p-identity/1-8/$1"
 r301 %r{/rabbitmq-cf/(?![\d-]+)(.*)}, "/rabbitmq-cf/1-7/$1"
-r301 %r{/rabbitmq-cf/1-7-6/(.*)}, "/rabbitmq-cf/1-7/$1"
+r301 %r{/rabbitmq-cf/1-7-[\d]+/(.*)}, "/rabbitmq-cf/1-7/$1"
+r301 %r{/rabbitmq-cf/1-6-[\d]+/(.*)}, "/rabbitmq-cf/1-6/$1"
+r301 %r{/rabbitmq-cf/1-5-[\d]+/(.*)}, "/rabbitmq-cf/1-5/$1"
+
 r301 %r{/bosh/(.*)}, 'http://bosh.io/docs/$1'
 r301 %r{/jmx-bridge/(?![\d-]+)(.*)}, "/jmx-bridge/1-8/$1"
 r301 %r{/p-mysql/(?![\d-]+)(.*)}, "/p-mysql/1-8/$1"
