a9fda493430051fe721d8b79bb3a015abda0e7f6
animatedmax <mhufnagel@pivotal.io>
2017-03-02 12:38:19 -0800

    fix redirects

--- redirects.rb
+++ redirects.rb
@@ -133,20 +133,20 @@ r301 '/spring-cloud-services/service-registry/consuming-a-service.html', '/sprin
 r301 '/spring-cloud-services/service-registry/registering-a-service.html', '/spring-cloud-services/service-registry/writing-client-applications.html'
 r301 '/spring-cloud-services/service-registry/background.html', '/spring-cloud-services/service-registry/oss.html'
 
-r301 %r{/spring-cloud-services/circuit-breaker/(.*)}, "/spring-cloud-services/circuit-breaker/common/$1"
-r301 %r{/spring-cloud-services/config-server/(.*)}, "/spring-cloud-services/config-server/common/$1"
-r301 %r{/spring-cloud-services/service-registry/(.*)}, "/spring-cloud-services/service-registry/common/$1"
+r301 %r{/spring-cloud-services/1-3/circuit-breaker/(.*)}, "/spring-cloud-services/1-3/common/circuit-breaker/$1"
+r301 %r{/spring-cloud-services/1-3/config-server/(.*)}, "/spring-cloud-services/1-3/common/config-server/$1"
+r301 %r{/spring-cloud-services/1-3/service-registry/(.*)}, "/spring-cloud-services/1-3/common/service-registry/$1"
 
-r301 '/spring-cloud-services/', '/spring-cloud-services/common/'
-r301 '/spring-cloud-services/index.html', '/spring-cloud-services/common/index.html'
-r301 '/spring-cloud-services/security-overview.html', '/spring-cloud-services/common/security-overview.html'
-r301 '/spring-cloud-services/client-dependencies.html', '/spring-cloud-services/common/client-dependencies.html'
-r301 '/spring-cloud-services/client-troubleshooting.html', '/spring-cloud-services/common/client-troubleshooting.html'
-r301 '/spring-cloud-services/service-instance-upgrades.html', '/spring-cloud-services/common/service-instance-upgrades.html'
+r301 '/spring-cloud-services', '/spring-cloud-services/common/'
+r301 '/spring-cloud-services/1-3/index.html', '/spring-cloud-services/1-3/common/index.html'
+r301 '/spring-cloud-services/1-3/security-overview.html', '/spring-cloud-services/1-3/common/security-overview.html'
+r301 '/spring-cloud-services/1-3/client-dependencies.html', '/spring-cloud-services/1-3/common/client-dependencies.html'
+r301 '/spring-cloud-services/1-3/client-troubleshooting.html', '/spring-cloud-services/1-3/common/client-troubleshooting.html'
+r301 '/spring-cloud-services/1-3/service-instance-upgrades.html', '/spring-cloud-services/1-3/common/service-instance-upgrades.html'
 
 r301 %r{/spring-cloud-services/shared/(?![\d-]+)(.*)}, "/spring-cloud-services/common/$1"
 
-r301 %r{/spring-cloud-services/(?![\d-]+)(.+)}, "/spring-cloud-services/1-3/$1"
+r301 %r{/spring-cloud-services/(?![\d-]+)(.*+)}, "/spring-cloud-services/1-3/$1"
 
 r301 '/owners.html', 'http://docs-owners-app-staging.cfapps.io'
 
