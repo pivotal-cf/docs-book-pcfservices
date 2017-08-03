9b96d795f237c504508fb9a66f3e6691833f33a3
animatedmax <mhufnagel@pivotal.io>
2017-08-03 16:27:31 -0700

    remove temporary 'edge' redirect

--- redirects.rb
+++ redirects.rb
@@ -191,6 +191,5 @@ r301 %r{/splunk/(.*)}, "/partners/splunk/$1"
 r301 %r{/tibco-businessworks/(.*)}, "/partners/tibco-businessworks/$1"
 
 r301 %r{/redis/syslog.html}, '/redis/1-9/installing.html#syslog'
-r301 %r{/edge/p-mysql/(.*)}, "/p-mysql/1-9/$1"
 
 r301 %r{/concourse-olm/(.*)}, "/p-concourse/$1"
