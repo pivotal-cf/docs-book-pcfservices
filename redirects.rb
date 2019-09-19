0297bf61bc6023d2ac49946b815d34fd7b497833
Landon Jarzynski <ljarzynski@pivotal.io>
2019-09-19 16:03:42 -0700

    windows redirects

--- redirects.rb
+++ redirects.rb
@@ -3,6 +3,12 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
+# PASW redirects for /platform/application-service-windows to old way
+r301 %r{/platform/application-service-windows/2-6/(.*)}, "/pivotalcf/2-6/windows/$1"
+r301 %r{/platform/application-service-windows/2-5/(.*)}, "/pivotalcf/2-5/windows/$1"
+r301 %r{/platform/application-service-windows/2-4/(.*)}, "/pivotalcf/2-4/windows/$1"
+r301 %r{/platform/application-service-windows/2-3/(.*)}, "/pivotalcf/2-3/windows/$1"
+
 # OM redirects for /platform/ops-manager to old way
 r301 %r{/platform/ops-manager/2-6/(.*)}, "/pivotalcf/2-6/om/$1"
 r301 %r{/platform/ops-manager/2-5/(.*)}, "/pivotalcf/2-5/om/$1"
