441912e33855bfcb0cd0770eba0dd1792ca8315d
Landon Jarzynski <ljarzynski@pivotal.io>
2019-10-22 11:48:30 -0700

    remove stray f from redirect file. f '

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,4 @@
- f# Redirect all production http traffic to https
+# Redirect all production http traffic to https
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
