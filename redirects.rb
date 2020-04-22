263d18347fbf94c0e72619aa76d00205ecba158a
animatedmax <mhufnagel@pivotal.io>
2020-04-22 09:59:15 -0700

    [#172421346] Add redirect: PAS Kubernetes to newest TAS Kubernetes

--- redirects.rb
+++ redirects.rb
@@ -754,3 +754,6 @@ r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-1-0/$1"
 # Redirect unversioned and 'latest' calls for application-service-windows/ to 2-9
 r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/platform/application-service-windows/2-9/$1"
 r301 %r{/platform/application-service-windows/latest/(.*)}, "/platform/application-service-windows/2-9/$1"
+
+# Redirect PAS Kubernetes 2.6.0 alpha to current version of TAS Kubernetes
+301 %r{/pas-kubernetes/2-6-0-alpha-1/(.*)}, '/tas-kubernetes/$1'
