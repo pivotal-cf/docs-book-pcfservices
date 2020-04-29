203ccb37ef57f48e342834d21d68f667e9a1fe9d
Severine Tymon <s.tymon@gmail.com>
2020-04-29 12:53:12 -0700

    update redirect and home page link [#172583020]

--- redirects.rb
+++ redirects.rb
@@ -571,7 +571,7 @@ r301 %r{/pks/latest/(.*)}, "/pks/1-6/$1"
 r301 %r{/pks/(?![\d-]+)(.*)}, "/pks/1-6/$1"
 
 # Redirect PRA docs
-r301 %r{/pra/(?![\d-]+)(.*)}, "/pra/2-2/$1"
+r301 %r{/pra/(?![\d-]+)(.*)}, "/pra/2-3/$1"
 
 # Redirect 'archives' (contains PDFs)
 r301 %r{/archives/(.*)}, "https://resources.docs.pivotal.io/pdfs/$1"
