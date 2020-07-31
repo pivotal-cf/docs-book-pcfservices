3544873e345763c15e3a005353579cdd727a55d2
jncd <jday@pivotal.io>
2020-07-31 12:52:30 -0700

    [#173921936]redirects for app-service-windows 2-10

--- redirects.rb
+++ redirects.rb
@@ -13,6 +13,7 @@ r301 %r{/platform/2-7/pcf-release-notes/(.*)}, '/platform/2-7/release-notes/$1'
 r301 %r{/pivotalcf/2-7/windows/(.*)}, "/platform/application-service-windows/2-7/$1"
 r301 %r{/pivotalcf/2-8/windows/(.*)}, "/platform/application-service-windows/2-8/$1"
 r301 %r{/pivotalcf/2-9/windows/(.*)}, "/platform/application-service-windows/2-9/$1"
+r301 %r{/pivotalcf/2-10/windows/(.*)}, "/platform/application-service-windows/2-10/$1"
 r301 %r{/platform/application-service-windows/2-6/(.*)}, "/pivotalcf/2-6/windows/$1"
 r301 %r{/platform/application-service-windows/2-5/(.*)}, "/pivotalcf/2-5/windows/$1"
 r301 %r{/platform/application-service-windows/2-4/(.*)}, "/pivotalcf/2-4/windows/$1"
@@ -564,8 +565,8 @@ r301 %r{/pas-kubernetes/2-6-0-alpha-1/(.*)}, '/tas-kubernetes/$1'
 r301 %r{/build-service/(?![\d-]+)(.*)}, "/build-service/0-2-0/$1"
 
 # Redirect unversioned and 'latest' calls for application-service-windows/ to 2-9
-r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/platform/application-service-windows/2-9/$1"
-r301 %r{/platform/application-service-windows/latest/(.*)}, "/platform/application-service-windows/2-9/$1"
+r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/platform/application-service-windows/2-10/$1"
+r301 %r{/platform/application-service-windows/latest/(.*)}, "/platform/application-service-windows/2-10/$1"
 # Redirect Healthwatch
 r301 %r{/pcf-healthwatch/(.*)}, '/platform/healthwatch/$1'
 
