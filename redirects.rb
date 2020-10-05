1235d131651c3ef2030110a252948a682f74dae1
Samia Nneji <snneji@pivotal.io>
2020-10-05 15:14:23 +0100

    Update redirect for TSMGR [#174970679]

--- redirects.rb
+++ redirects.rb
@@ -674,8 +674,8 @@ r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-9/$1"
 
 r301 %r{/pks/1-1/concourse-helm.html}, '/pks/1-2/helm.html'
 
-# Redirect for Kubernates Service Manager
-r301 %r{/tanzu-service-manager/(?![\d-]+)(.*)}, "/tanzu-service-manager/0-11/$1"
+# Redirect for Tanzu Service Manager
+r301 %r{/tanzu-service-manager/(?![\d-]+)(.*)}, "/tanzu-service-manager/1-0/$1"
 r301 %r{/ksm/(.*)}, '/tanzu-service-manager/$1'
 
 # Redirects for Metrics
