d0f3c8e23ad711fa1ab03b9bf129f1719daa2dee
Oliver Graves <ograves@pivotal.io>
2021-01-29 10:16:00 -0800

    [#176710663] redirection to TKGI v1.10: Release Day +1

--- redirects.rb
+++ redirects.rb
@@ -642,12 +642,12 @@ r301 %r{/pks/1-2/(.*)}, 'https://resources.docs.pivotal.io/pdfs/pks-1-2.pdf'
 
 # Redirect non-versioned and 'latest' URL calls to the most recent version of PKS
 r301 %r{/runtimes/pks/(.*)}, "/tkgi/$1"
-r301 %r{/runtimes/pks/latest/(.*)}, "/tkgi/1-9/$1"
-r301 %r{/runtimes/pks/(?![\d-]+)(.*)}, "/tkgi/1-9/$1"
-r301 %r{/tkgi/(?![\d-]+)(.*)}, "/tkgi/1-9/$1"
-r301 %r{/tkgi/latest/(.*)}, "/tkgi/1-9/$1"
-r301 %r{/pks/(?![\d-]+)(.*)}, "/tkgi/1-9/$1"
-r301 %r{/pks/latest/(.*)}, "/tkgi/1-9/$1"
+r301 %r{/runtimes/pks/latest/(.*)}, "/tkgi/1-10/$1"
+r301 %r{/runtimes/pks/(?![\d-]+)(.*)}, "/tkgi/1-10/$1"
+r301 %r{/tkgi/(?![\d-]+)(.*)}, "/tkgi/1-10/$1"
+r301 %r{/tkgi/latest/(.*)}, "/tkgi/1-10/$1"
+r301 %r{/pks/(?![\d-]+)(.*)}, "/tkgi/1-10/$1"
+r301 %r{/pks/latest/(.*)}, "/tkgi/1-10/$1"
 
 # Redirect PRA docs
 r301 %r{/pra/(?![\d-]+)(.*)}, "/pra/3-0/$1"
