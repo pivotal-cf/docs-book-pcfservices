3042454d154c76c92c723d088430a4fbae6ca090
animatedmax <mhufnagel@pivotal.io>
2021-02-17 09:55:50 -0800

    redirect test

--- redirects.rb
+++ redirects.rb
@@ -1,6 +1,6 @@
 # Redirect all production http traffic to https
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
-  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+  rack_env['SERVER_NAME'] == rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
 r301 %r{/bosh/(.*)}, 'http://bosh.io/docs/$1'
