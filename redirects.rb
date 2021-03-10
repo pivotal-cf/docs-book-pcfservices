97bd6f860af196721a3bd460f31bc9dc71cdc8a4
Miranda Limonczenko <mlimonczenko@pivotal.io>
2021-03-09 16:56:29 -0800

    Move redirect trying to fix it[#176754915]

--- redirects.rb
+++ redirects.rb
@@ -290,7 +290,7 @@ r301 %r{/platform/(2-[7-9]|2-\d\d+)/opsguide/(.*)}, '/ops-manager/$1/opsguide/$2
 r301 %r{/pivotalcf/(2-[3-6])/upgrade/checklist.html}, '/pivotalcf/$1/upgrading/checklist.html'
 
 # All other /ops-manager redirects
-r301 %r{/ops-manager/(\d-\d+)/install/ops-man.html}, '/ops-manager/$1/index.html'
+
 r301 %r{/ops-manager/(?![\d-]+)(.*)}, '/ops-manager/2-10/$1'
 r301 %r{/platform/ops-manager/(.*)}, '/ops-manager/$1'
 r301 %r{/platform/ops-manager/(2-[7-9]|2-\d\d+)/(.*)}, '/ops-manager/$1/$2'
@@ -300,6 +300,7 @@ r301 %r{/platform/om/(.*)}, '/ops-manager/2-10/$1'
 r301 %r{/pcf/om/(?![\d-]+)/(.*)}, "/ops-manager/2-10/$1"
 r301 %r{/pcf/om/(2-[7-9]|2-\d\d+)/(.*)}, "/ops-manager/$1/$2"
 r301 %r{/pcf/om/(2-[3-6])/(.*)}, '/pivotalcf/$1/$2'
+r301 %r{/ops-manager/(\d-\d+)/install/ops-man.html}, '/ops-manager/$1/index.html'
 
 
 # CredHub redirects
