8e3b82e0755e5ba3a98818c671766d91eb074fbd
Lena Hunter <lhunter@pivotal.io>
2021-04-20 21:16:58 -0700

    redirect test for gpupgrade

--- redirects.rb
+++ redirects.rb
@@ -474,6 +474,8 @@ r301 %r{/43240/(.*)}, 'http://gpdb.docs.pivotal.io/43240/$1'
 r301 %r{/5(.*)}, 'http://gpdb.docs.pivotal.io/5$1'
 r301 %r{/6-(.*)}, 'http://gpdb.docs.pivotal.io/6-$1'
 
+# For gpupgrade
+r301 %r{/upgrade/(.*)}, 'http://gpdb.docs.pivotal.io/upgrade/$1'
 
 # For GPCC
 r301 '/supported-platforms/gpcc.html/', 'https://gpcc.docs.pivotal.io/supported-platforms/gpcc.html'
