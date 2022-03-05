1b6d003bc1365f99f00b63a05fa56d80200ecc55
animatedmax <mhufnagel@pivotal.io>
2022-03-04 16:54:28 -0800

    [#181426281] redirect from docs.pivotal.io to docs.vmware.com

--- redirects.rb
+++ redirects.rb
@@ -769,7 +769,8 @@ r302 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-alpha/$1"
 r301 %r{/event-alerts/(?![\d-]+)(.*)}, "/event-alerts/1-2/$1"
 
 # Redirect for FIM
-r302 %r{/addon-fim/(?![\d-]+)(.*)}, "/addon-fim/2-1/$1"
+r302 %r{/addon-fim/(?![\d-]+)(.*)}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/2.1/fim/GUID-index.html"
+r301 %r{/addon-fim/2-1/(.*)}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/2.1/fim/GUID-index.html"
 
 # Redirect for IPsec
 r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-9/$1"
