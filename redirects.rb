9ccb5403743ec6cfe28c4c9dd08950a01e0dabca
Ben Klein <bklein@vmware.com>
2022-08-05 15:46:04 -0500

    [#182919436] Update redirects for 2.13

--- redirects.rb
+++ redirects.rb
@@ -15,8 +15,8 @@ r301 %r{/platform/(2-[7-9]|2-\d\d+)/index.html}, '/ops-manager/$1/install/index.
 # Release Notes and Breaking Changes moved to product books
 # Versionless release notes default to latest GA version
 r301 %r{/platform/(?![\d-]+)/release-notes/opsmanager-rn.html}, '/ops-manager/2-10/release-notes.html'
-r301 %r{/platform/(?![\d-]+)/release-notes/(runtime-rn.html|segment-rn.html)}, '/application-service/2-12/release-notes/$2'
-r301 %r{/platform/(?![\d-]+)/release-notes/windows-rn.html}, '/application-service/2-12/windows/release-notes.html'
+r301 %r{/platform/(?![\d-]+)/release-notes/(runtime-rn.html|segment-rn.html)}, '/application-service/2-13/release-notes/$2'
+r301 %r{/platform/(?![\d-]+)/release-notes/windows-rn.html}, '/application-service/2-13/windows/release-notes.html'
 
 # General 2-7 to 2-xx release note and breaking change redirects
 r301 %r{/platform/(2-[7-9]|2-\d\d+)/release-notes/breaking-changes.html(.*)}, '/ops-manager/$1/release-notes.html$2'
@@ -52,10 +52,10 @@ r301 %r{/pivotalcf/(2-[0-2])/overview/release-notes/(.*)}, '/pivotalcf/$1/pcf-re
 r301 %r{/pivotalcf/(1-\d+|1-\d\d+)/overview/release-notes/(.*)}, '/pivotalcf/$1/pcf-release-notes/$2'
 
 # Redirects for Windows to /application-service/windows
-r301 %r{/application-service-windows/latest/(.*)}, '/application-service/2-12/windows/$1'
-r301 %r{/application-service-windows/(?![\d-]+)(.*)}, '/application-service/2-12/windows/$1'
+r301 %r{/application-service-windows/latest/(.*)}, '/application-service/2-13/windows/$1'
+r301 %r{/application-service-windows/(?![\d-]+)(.*)}, '/application-service/2-13/windows/$1'
 r301 %r{/platform/application-service-windows/latest/(.*)}, '/application-service/2-11/windows/$1'
-r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, '/application-service/2-12/windows/$1'
+r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, '/application-service/2-13/windows/$1'
 r301 %r{/platform/application-service-windows/(2-[7-9]|2-\d\d+)/(.*)}, '/application-service/$1/windows/$2'
 r301 %r{/platform/application-service-windows/(2-[3-6])/(.*)}, '/pivotalcf/$1/windows/$2'
 r301 %r{(/pivotalcf|/platform)/(2-[7-9]|2-\d\d+)/windows/(.*)}, '/application-service/$2/windows/$3'
@@ -256,8 +256,8 @@ r301 %r{/platform/(2-[7-9]|2-\d\d+)/upgrading/configuring.html}, '/application-s
 
 # All other /application-service redirects
 r301 %r{/platform/application-service/(2-[7-9]|2-\d\d+)/(.*)}, '/application-service/$1/$2'
-r301 %r{/platform/application-service/(?![\d-]+)(.*)}, '/application-service/2-12/$1'
-r301 %r{/application-service/(?![\d-]+)(.*)}, '/application-service/2-12/$1'
+r301 %r{/platform/application-service/(?![\d-]+)(.*)}, '/application-service/2-13/$1'
+r301 %r{/application-service/(?![\d-]+)(.*)}, '/application-service/2-13/$1'
 
 
 # Ops Manager redirects
@@ -721,7 +721,7 @@ r301 %r{/platform/healthwatch/(.*)}, '/healthwatch/$1'
 r301 %r{/platform/(2-[7-9]|2-\d\d+)/healthwatch/(.*)}, '/healthwatch/$1'
 
 # Reinstate versionless pivotalcf link
-r301 %r{/pivotalcf/(?![\d-]+)(.*)}, '/platform/2-12/$1'
+r301 %r{/pivotalcf/(?![\d-]+)(.*)}, '/platform/2-13/$1'
 
 # Reinstate versionless platform links
 r301 %r{/platform/(?![\d-]+)(.*)}, '/platform/2-11/$1'
